1b98466bd9eefdec9389cd6bd3ba427308068745
gregoirepuget <gregoirepuget@gmail.com>
2018-06-05 10:52:33 +0200

    Création de la classe et instanciation

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,2 @@
-
+let slider1 = new HeticSlider(".slider1", 3000, true, true)
+let slider2 = new HeticSlider(".slider2", 5000, false, true)
--- scripts/heticSlider.js
+++ scripts/heticSlider.js
@@ -1,7 +1,14 @@
  class HeticSlider{
-   constructor()
+   constructor(container,speed,pagination,arrow)
    {
-     
+     this.container = container
+     this.speed = speed
+     this.transition = "ease-in"
+     this.imagesNumber = 0
+     this.currentImage = 0
+     this.direction = "right"
+     this.pagination = pagination
+     this.arrow = arrow
    }
 
  }
